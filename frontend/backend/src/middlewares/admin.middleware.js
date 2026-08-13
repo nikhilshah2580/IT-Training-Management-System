18f2c13852deca55946089367f7e590869a66401
Nikhil Raj Sah <nikhilshah98198@gmail.com>
2026-08-13 15:47:44 +0545

    remove unwanted copy in frontend backend file

--- frontend/backend/src/middlewares/admin.middleware.js
+++
@@ -1,9 +0,0 @@
-export const verifyAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
-    const err = new Error("Access denied. Admin only.");
-    err.statusCode = 403;
-    throw err;
-  }
-
-  next();
-};
